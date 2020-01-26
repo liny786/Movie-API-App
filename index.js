a6e5d90cd3a48ceb2a6b683c6e37fe359d0650fc
furl726 <incredibleayush81@gmail.com>
2020-01-26 17:30:33 +0530

    added index file

--- index.js
+++ index.js
@@ -2,23 +2,24 @@ var express = require("express");
 var request = require("request");
 
 var app = express();
-app.set("view engine", ejs);
+app.set("view engine", "ejs");
 
 app.get("/", function(req, res){
     res.render("search");
 });
 
-app.get("/results", function(req, res){
+app.get("/results", function(req, res) {
 
     var query = req.query.search;
     var url = "http://www.omdbapi.com/?apikey=[2e904546]&s=" + query;
-    request("url", function(error, response, body){
-        if(!error && response.statuscode==200)
+    request("url", function (error, response, body) {
+        if (!error && response.statusCode == 200)
             var results = JSON.parse(body);
-        res.render("results", {results:results});
+           console.log(body);
+        res.render("results", {results: results});
     });
-
-    app.listen(9000, function(req, res){
+});
+    app.listen(9000, function(){
         console.log("Server has started");
     });
 
